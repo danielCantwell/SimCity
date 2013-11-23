87cd00259ffb48fb84e4d04e89d7639932ebdcaa
danielCantwell <dcantwel@usc.edu>
2013-11-22 18:30:54 -0800

    Created MockHousingPerson

--- simcity201/src/housing/test/mock/MockHousingPerson.java
+++ simcity201/src/housing/test/mock/MockHousingPerson.java
@@ -6,16 +6,16 @@ package housing.test.mock;
 import restaurant.test.mock.EventLog;
 import restaurant.test.mock.LoggedEvent;
 import SimCity.Base.Person;
-import SimCity.Base.Person.PersonState;
 import SimCity.Globals.Money;
 import SimCity.gui.Gui;
-import housing.interfaces.HousingPerson;
 
 /**
  * @author Daniel
  * 
  */
 public class MockHousingPerson extends Person {
+	
+	public EventLog log = new EventLog();
 
 	public MockHousingPerson(Gui gui, String mainRole, Vehicle vehicle,
 			Morality morality, Money money, Money moneyThresh, int hunger,
@@ -25,9 +25,39 @@ public class MockHousingPerson extends Person {
 	}
 	
 	@Override
+	public void setHomeType(String home) {
+		log.add(new LoggedEvent("Received setHomeType from tenant. Type = " + home ));
+		super.setHomeType(home);
+	}
+	
+	@Override
 	public int getHungerLevel() {
-		// TODO Auto-generated method stub
+		log.add(new LoggedEvent("Received getHungerLevel from tenant."));
 		return super.getHungerLevel();
+	}
+	
+	@Override
+	public int getHungerThreshold() {
+		log.add(new LoggedEvent("Received getHungerThreshold from tenant."));
+		return super.getHungerThreshold();
+	}
+	
+	@Override
+	public PersonState getPersonState() {
+		log.add(new LoggedEvent("Received getPersonState from tenant."));
+		return super.getPersonState();
+	}
+	
+	@Override
+	public Money getMoney() {
+		log.add(new LoggedEvent("Received getMoney from tenant."));
+		return super.getMoney();
+	}
+	
+	@Override
+	public Money getMoneyThreshold() {
+		log.add(new LoggedEvent("Received getMoneyThreshold from tenant."));
+		return super.getMoneyThreshold();
 	}
 
 }
