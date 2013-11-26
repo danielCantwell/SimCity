6877c7e575eb1bdcce5fb01df0b67757a27d1422
jchand <jchand@usc.edu>
2013-11-25 18:44:39 -0800

    Added graphics and animations (initial).

--- simcity201/src/exterior/gui/Gui.java
+++ simcity201/src/exterior/gui/Gui.java
@@ -6,4 +6,8 @@ public interface Gui {
     public void updatePosition();
     public void draw(Graphics2D g);
     public boolean isPresent();
+    public int getX();
+    public int getY();
+    public int getRotation();
+    public final boolean SHOW_RECT = false;
 }
