688b3cc0d773b24cb188b95c18fd3bfec0e92cad
Timothy So <Gyro3218@aol.com>
2013-12-07 20:20:04 -0800

    Made gui less resolution dependent.

--- simcity201/src/exterior/gui/SetupPanel.java
+++ simcity201/src/exterior/gui/SetupPanel.java
@@ -146,7 +146,7 @@ public class SetupPanel extends JFrame {
 		animationPanel = ap;
 
 		setSize(WINDOWX, WINDOWY);
-		setBounds(0, 640, WINDOWX, WINDOWY);
+		setBounds(0, 0, WINDOWX, WINDOWY);
 		setVisible(true);
 		setTitle("Setup");
 		setLayout(new BorderLayout());
