c6204ddb47406fb0eb0d1d3b081c67df1b3b236d
Timothy So <Gyro3218@aol.com>
2013-12-10 00:49:49 -0800

    Preventing null pointer exception from non-instantiated money.

--- simcity201/src/timRest/TimCashierRole.java
+++ simcity201/src/timRest/TimCashierRole.java
@@ -37,7 +37,7 @@ public class TimCashierRole extends Role implements TimCashier, MarketDeliveryCa
 	public TimCashierRole()
 	{
 		super();
-		//cashInRegister = new Money(300, 0);
+		cashInRegister = new Money(0, 0);
 	}
 	
 	public void msgHereIsACheck(TimWaiter waiter, String choice, int tableNumber)
